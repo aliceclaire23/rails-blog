8da76457866304775d4d90a39ee60035e9566d13
Jonic Linley <jonic@100yen.co.uk>
2023-02-24 20:33:49 +0000

    Use Rails time_ago_in_words

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,27 +4,6 @@ class BlogPost < ApplicationRecord
 
   scope :published, -> { where("published_at < '#{Time.now}'") }
 
-  def time_since_published
-    old = published_at
-    s = (Time.now - old).round
-    m = s/60
-    h = m/60
-    d = h/24
-    w = d/7
-    y = w/52.17
 
-    if s < 60
-      "#{s.round} seconds"
-    elsif m < 60
-      "#{m.round} minutes"
-    elsif h < 24
-      "#{h.round} hours"
-    elsif d < 7
-      "#{d.round} days"
-    elsif w < 52.17
-      "#{w.round} weeks"
-    else
-      "#{y.round} years"
-    end
   end
 end
