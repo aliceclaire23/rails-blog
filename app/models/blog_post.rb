3b8b30b25cb32fdec20cb392c7eeb0a7ee0148e4
Jonic Linley <jonic@100yen.co.uk>
2023-02-24 20:34:37 +0000

    Update scope query string interpolation

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -2,7 +2,7 @@ class BlogPost < ApplicationRecord
   belongs_to(:author)
   validates(:title, :content, presence: true)
 
-  scope :published, -> { where("published_at < '#{Time.now}'") }
+  scope :published, -> { where("published_at < ?", Time.zone.now) }
 
   before_create :set_published_at
 
